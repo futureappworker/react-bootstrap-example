693309eaeb7d2113443255e887bd86beeed5517a
ben <sherry@sherrydeMacBook-Air.local>
2022-11-04 10:26:16 +0800

    Add bootstrap scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import '../styles/customTheme.scss';
+import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line
